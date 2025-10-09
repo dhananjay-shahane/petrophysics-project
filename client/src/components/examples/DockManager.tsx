b76a1a0cdb50dc39ef91143c0637a5fe2d35468b
Dhananjay Shahane <143813959+DhananjayShahane@users.noreply.github.com>
2025-10-09 11:38:51 +0530

    Remove all files

--- client/src/components/examples/DockManager.tsx
+++
@@ -1,5 +0,0 @@
-import DockManager from '../DockManager';
-
-export default function DockManagerExample() {
-  return <DockManager />;
-}
--- client/src/components/examples/ZonationPanel.tsx
+++
@@ -1,5 +0,0 @@
-import ZonationPanel from '../ZonationPanel';
-
-export default function ZonationPanelExample() {
-  return <ZonationPanel onClose={() => console.log('Close zonation panel')} />;
-}
